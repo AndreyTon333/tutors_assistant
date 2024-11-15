6992be035f9365dd829e50f9c3a0d98d9a967070
Andrey Ton <ton333@mail.ru>
2024-11-15 16:42:03 +0300

    input_file

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,5 +1,5 @@
 from aiogram import Router
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
 router = Router()
 import logging
 
@@ -8,6 +8,18 @@ import logging
 @router.message()
 async def send_answer_other(message: Message):
     logging.info('send_answer_other')
+    if message.text == '/get_logfile':
+        logging.info(f'all_message message.admin./get_logfile')
+        file_path = "py_log.log"
+        await message.answer_document(FSInputFile(file_path))
+        return
+
+    if message.text == '/get_dbfile':
+        logging.info(f'all_message message.admin./get_dbfile')
+        file_path = "database/db.sqlite3"
+        await message.answer_document(FSInputFile(file_path))
+        return
+
     await message.answer(text=f'❌ <b>Неизвестная команда!</b>\n'
                               f'Обновите Меню, нажав /start')#,
                                #parse_mode='html')
